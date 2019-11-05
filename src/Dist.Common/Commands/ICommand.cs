3f6ae5fddbc55c8151c80f3490ef8924f733c458
Hudson Lima <hudsonlima.ti@gmail.com>
2019-11-04 23:00:35 -0300

    Adding the command structure

---
+++ src/Dist.Common/Commands/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dist.Common.Commands
+{
+    //Marker Interface    
+    public interface ICommand
+    {
+    }
+}
---
+++ src/Dist.Common/Commands/ICommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using Dist.Common.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dist.Common
+{
+    public interface ICommandHandler<in T> where T : ICommand
+    {
+        Task HandleAsync(T command);
+    }
+}
